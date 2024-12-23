da2cb2edb98e26f9986fee003a9ea272c35b3214
Thomas Lefebvre <113672755+Tomek62@users.noreply.github.com>
2024-12-23 14:40:39 +0100

    Update streamlit_app.py
    Add panda df to use search_on column for api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ st.write(
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+pd_df = my_dataframe.to_pandas()
 
 name_on_order = st.text_input('Name on Smoothie:')
 
@@ -25,8 +26,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string+=fruit+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit,' is ', search_on, '.')
         st.subheader(fruit + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     st.text(ingredients_string[:-2])
 
